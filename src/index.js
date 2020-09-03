b8aaa85b3e81bfb20a6de695c54a79d0cd326e58
chenyaxiong <403700030@qq.com>
2020-09-03 23:13:49 +0800

    开始学习loader

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var Header = require('./header');
+var Content = require('./content');
+var Sidebar = require('./sidebar');
+var avatar = require('./avatar.jpg');
+
+console.log(avatar)
+
+new Header();
+new Content();
+new Sidebar();
